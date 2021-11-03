96d7bc0ee64f38f26180a5bec682c3175bd23ec4
Prilla-dev <84512122+Prilla-dev@users.noreply.github.com>
2021-11-03 01:37:09 -0800

    image folder added

--- Screens/LoginDetails.js
+++ Screens/LoginDetails.js
@@ -9,7 +9,16 @@ export default function LoginDetails({navigation}){
                 <Image source={require('../assets/images/details.jpg')}
                 style={{width:"100%",height:"40%"}}
                 />
-            <Text>hi</Text>
+            <Text style={{
+                fontSize:28,
+                alignSelf:"center",
+                fontWeight:'bold'
+            }}
+            
+            
+            >My Account</Text>
+            <Text>Life just got better.<Text>The ultimate platform for everything you want to do</Text>
+            </Text>
             </View>
         )
         
